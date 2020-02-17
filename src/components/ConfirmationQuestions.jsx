11f03cdd233bf2d29afcc1e5f49191f6b8559757
Michael-Wayne-Wells <mwells1286@gmail.com>
2020-02-17 10:58:14 -0800

    update confirmation to show state

---
+++ src/components/ConfirmationQuestions.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function ConfirmationQuestions(props){
+  return (
+    <div>
+      <p>Have you gone through all the steps on the Learn How to Program debugging lesson?</p>
+      <button onClick={props.onTroubleShootingConfirmation}>Yes</button>
+      </div>
+  );
+}
+  ConfirmationQuestions.propTypes = {
+    onTroubleShootingConfirmation: PropTypes.func
+  };
