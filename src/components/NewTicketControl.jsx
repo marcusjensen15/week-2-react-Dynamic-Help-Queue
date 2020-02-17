75c94362a07d0cb8df146bd077eb39fe1ce48d2d
Michael-Wayne-Wells <mwells1286@gmail.com>
2020-02-17 10:57:07 -0800

    update form for state

---
+++ src/components/NewTicketControl.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ConfirmationQuestions from './ConfirmationQuestions';
+import NewTicketForm from './NewTicketForm';
+import PropTypes from 'prop-types';
+
+
+class NewTicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+    this.handleTroubleShootingConfirmation = this.handleTroubleShootingConfirmation.bind(this);
+  }
+
+  handleTroubleShootingConfirmation(){
+    this.setState({formVisibleOnPage: true});
+
+  }
+
+  render(){
+    let currentlyVisibleContent = null;
+    if (this.state.formVisibleOnPage){
+      currentlyVisibleContent = <NewTicketForm onNewTicketCreation={this.props.onNewTicketCreation}/>;
+    }else{
+      currentlyVisibleContent = <ConfirmationQuestions onTroubleShootingConfirmation={this.handleTroubleShootingConfirmation}/>
+    }
+    return (
+      <div>
+      {currentlyVisibleContent}
+      </div>
+
+    );
+  }
+}
+
+NewTicketControl.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
+
+export default NewTicketControl;
